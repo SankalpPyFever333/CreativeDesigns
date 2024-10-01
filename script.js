762866cc98bf69de24beb7061e3cda47da53b26c
sankalp <sankalppandey696@gmail.com>
2024-10-02 01:50:48 +0530

    uses scrollTrigger for animation

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+// gsap.to(".vdodiv", {
+//   scrollTrigger: {
+//     trigger: ".home",
+//     start: "top top",
+//     end: "bottom top",
+//     scrub: 2,
+//     pin: true,
+//     markers: true,
+//   },
+//   "--clip": "0%",
+//   duration: 2,
+// });
+
+
+
