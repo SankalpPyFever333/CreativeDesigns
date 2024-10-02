9745f56143331c4ad19abe26f367c13a8b185ade
sankalp <sankalppandey696@gmail.com>
2024-10-02 16:27:42 +0530

    created animation for home page and real parts

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
+gsap.set(".slidesm", {
+  scale: 5,
+});
 
+function homepageAnimation() {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".home",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 4,
+      // pin: true,
+      markers: true,
+    },
+  });
 
+  tl.to(
+    ".vdodiv",
+    {
+      "--clip": "0%",
+    },
+    "a"
+  )
+    .to(
+      ".slidesm",
+      {
+        scale: 1,
+      },
+      "a"
+    )
+    .to(
+      ".lft",
+      {
+        xPercent: -10,
+      },
+      "b"
+    )
+    .to(
+      ".rgt",
+      {
+        xPercent: 10,
+      },
+      "b"
+    );
+}
 
-// gsap.to(".vdodiv", {
-//   scrollTrigger: {
-//     trigger: ".home",
-//     start: "top top",
-//     end: "bottom top",
-//     scrub: 2,
-//     pin: true,
-//     markers: true,
-//   },
-//   "--clip": "0%",
-//   duration: 2,
-// });
+function realClassAnimation() {
+  gsap.to(".slide", {
+    scrollTrigger: {
+      trigger: ".real",
+      start: "top top",
+      end: "bottom top",
+      markers: true,
+      scrub: 2,
+    },
+    xPercent: -200,
+    ease: Power4,
+  });
+}
 
-
-
+homepageAnimation();
+realClassAnimation();
